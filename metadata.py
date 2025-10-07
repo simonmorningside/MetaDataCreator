9ba04c1d7def1de4101fde2bb8ece80fb6602f70
Simon Zychowski <saz002@morningside.edu>
2025-10-07 11:06:20 -0500

    Added both prints

--- metadata.py
+++ metadata.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 ctk = pd.read_csv('C:\\Users\\Morningside\\MetaDataCreator\\data\\CTK.csv')
-
+fnd = pd.read_csv('C:\\Users\\Morningside\\MetaDataCreator\\data\\FND.csv')
 print(ctk.info())
+print(fnd.info())
