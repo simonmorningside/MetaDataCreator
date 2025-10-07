2ecb5a383dab25372be68626b0aa9e77d1d860c6
Simon Zychowski <saz002@morningside.edu>
2025-10-07 11:16:58 -0500

    Pulling out info

--- metadata.py
+++ metadata.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 ctk = pd.read_csv('C:\\Users\\Morningside\\MetaDataCreator\\data\\CTK.csv')
 fnd = pd.read_csv('C:\\Users\\Morningside\\MetaDataCreator\\data\\FND.csv')
-print(ctk.info())
-print(fnd.info())
+print(ctk[['Identifier (add 4 zeros) no extension', 'Title']].info())
+print(fnd[['Identifier', 'Title']].info())
